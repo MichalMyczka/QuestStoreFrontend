4c05d77963a92d809451a166db6788ed48b22686
maciek <mcyran1996@gmail.com>
2020-09-16 11:55:00 +0200

    rework

---
+++ js/login.js
@@ -0,0 +1,36 @@
+
+const form = document.querySelector("#login-form");
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const data = `email=${this.email.value}&password=${this.password.value}`;
+    console.log(data);
+    login(data);
+});
+
+function login(data) {
+    fetch("http://localhost:8000/login",
+        {
+            credentials: 'same-origin',
+            method: "POST",
+            body: data
+        })
+        .then(function (response) {
+            if (!response.ok) {
+                throw Error(response.statusText);
+            }
+            return response.json();
+        })
+        .then(function (user) {
+            // user is authenticated, and cookie send by server is set in browser
+            console.log(user);
+            if(user.role_ID ===3) {
+                window.location.href = "CreepPages/creepStartingPage.html";
+            }
+
+        }).catch(function (error) {
+        // user NOT authenticated, server return different status than 200-299
+            const message = document.querySelector(".message");
+            message.innerHTML = error;
+    });
+}
