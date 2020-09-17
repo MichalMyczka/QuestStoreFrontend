20075464263285dcaac4f98fa4fa4212ba340176
Michal Myczka <michalmorgoth@gmail.com>
2020-09-17 10:05:23 +0200

    mentor add quest front+back

---
+++ js/questAdd.js
@@ -0,0 +1,34 @@
+
+const form = document.querySelector("#questAdd-form");
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const data = `questName=${this.questName.value}&codecoinsEarned=${this.codecoinsEarned.value}&questDescription=${this.questDescription.value}`;
+    console.log(data);
+    addQuest(data);
+});
+
+function addQuest(data) {
+    fetch("http://localhost:8000/addQuest",
+        {
+            credentials: 'same-origin',
+            method: "POST",
+            body: data
+        })
+        .then(function (response) {
+            if (!response.ok) {
+                throw Error(response.statusText);
+            }
+            return response.json();
+        })
+        .then(function (quest) {
+
+            console.log(quest);
+
+
+        }).catch(function (error) {
+
+        const message = document.querySelector(".message");
+        message.innerHTML = error;
+    });
+}
