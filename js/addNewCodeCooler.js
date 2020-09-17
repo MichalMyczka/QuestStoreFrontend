7e06883f06ef723c6b645cf837bd6ba22fa5a5d4
Artur Jakubowski <a.jakubowskipl@gmail.com>
2020-09-17 15:04:31 +0200

    JS file and HTML file are now connected. JS see HTML and JSON files are logged in console

--- js/addNewCodeCooler.js
+++ js/addNewCodeCooler.js
@@ -10,11 +10,11 @@ form.addEventListener('submit', function (e) {
     &codeCoolerEmail=${this.codeCoolerEmail.value}`;
 
     console.log(data);
-    login(data);
+    addUser(data);
 });
 
-function login(data) {
-    fetch("http://localhost:8000/login",
+function addUser(data) {
+    fetch("http://localhost:8000/mentorAddUser",
         {
             credentials: 'same-origin',
             method: "POST",
@@ -26,26 +26,24 @@ function login(data) {
             }
             return response.json();
         })
-        .then(function (user) {
-            // user is authenticated, and cookie send by server is set in browser
-            console.log(user);
-            switch (user.role_ID) {
-                case 3:
-                    window.location.href = "CreepPages/creepStartingPage.html";
-                    break;
-                case 2:
-                    window.location.href = "MentorPages/mentorStartingPage.html";
-                    break;
-                case 1:
-                    window.location.href = "CodecoolerPages/codecoolerStartingPage.html"
-                    break;
-            }
+        .then(function (data) {
+            console.log(data);
+            // switch (user.role_ID) {
+            //     case 3:
+            //         window.location.href = "CreepPages/creepStartingPage.html";
+            //         break;
+            //     case 2:
+            //         window.location.href = "MentorPages/mentorStartingPage.html";
+            //         break;
+            //     case 1:
+            //         window.location.href = "CodecoolerPages/codecoolerStartingPage.html"
+            //         break;
+            // }
             // if(user.role_ID ===3) {
             //     window.location.href = "CreepPages/creepStartingPage.html";
             // }
 
         }).catch(function (error) {
-        // user NOT authenticated, server return different status than 200-299
         const message = document.querySelector(".message");
         message.innerHTML = error;
     });
