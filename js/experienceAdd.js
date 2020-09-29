32a1c1746897cd8fbced79f0f42bc223cb565d5a
Michal Myczka <michalmorgoth@gmail.com>
2020-09-29 10:56:30 +0200

    experience lvl and mentor creation done

---
+++ js/experienceAdd.js
@@ -0,0 +1,34 @@
+
+const form = document.querySelector("#experienceAdd-form");
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const data = `levelTitle=${this.levelTitle.value}&codepointAmount=${this.codepointAmount.value}`;
+    console.log(data);
+    addExperience(data);
+});
+
+function addExperience(data) {
+    fetch("http://localhost:8000/createExperienceLvl",
+        {
+            credentials: 'same-origin',
+            method: "POST",
+            body: data
+        })
+        .then(function (response) {
+            if (!response.ok) {
+                throw Error(response.statusText);
+            }
+            return response.json();
+        })
+        .then(function (experienceAdd) {
+
+            console.log(experienceAdd);
+
+
+        }).catch(function (error) {
+
+        const message = document.querySelector(".message");
+        message.innerHTML = error;
+    });
+}
---
+++ js/mentorAdd.js
@@ -0,0 +1,34 @@
+
+const form = document.querySelector("#mentorAdd-form");
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const data = `mName=${this.mName.value}&mSurname=${this.mSurname.value}&mEmail=${this.mEmail.value}&mPhone=${this.mPhone.value}&mPassword=${this.mPassword.value}&mClass=${this.mClass.value}`;
+    console.log(data);
+    addMentor(data);
+});
+
+function addMentor(data) {
+    fetch("http://localhost:8000/creepAddMentor",
+        {
+            credentials: 'same-origin',
+            method: "POST",
+            body: data
+        })
+        .then(function (response) {
+            if (!response.ok) {
+                throw Error(response.statusText);
+            }
+            return response.json();
+        })
+        .then(function (mentorAdd) {
+
+            console.log(mentorAdd);
+
+
+        }).catch(function (error) {
+
+        const message = document.querySelector(".message");
+        message.innerHTML = error;
+    });
+}
