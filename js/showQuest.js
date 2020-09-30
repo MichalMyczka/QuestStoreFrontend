03e94cb04f5bb82f7bd7ae2422d8166252f7ed2c
Michal Myczka <michalmorgoth@gmail.com>
2020-09-30 14:47:19 +0200

    quest tiles fix

--- js/showQuest.js
+++ js/showQuest.js
@@ -5,38 +5,52 @@ let questList = [];
 function questTile() {
     for (let i=0; i <= questList.length; i++){
 
-        let sel = document.getElementById("test");
+        let sel = document.getElementById("tiles");
+
         let questName =  questList[i]["questName"];
         let reward = "Reward: " + questList[i]["reward"];
-        let isBasic;
+        let isBasic = questList[i]["isBasic"];
 
         if(questList[i]["isBasic"] === true){
-            isBasic = "available"
+            isBasic = "Basic"
         }
         else{
-            isBasic = "not Available"
+            isBasic = "Extra"
         }
+        let isActive = questList[i]["isActive"];
 
         let div = document.createElement("div");
         div.classList.add("tile");
+
         let h3 = document.createElement("h3");
         h3.classList.add("tileName");
         h3.innerHTML = questName;
+
         let p1 = document.createElement("p");
         p1.classList.add("tileReward");
         p1.innerHTML = reward;
+
         let p2 = document.createElement("p");
         p2.classList.add("tileIsBasic");
         p2.innerHTML = isBasic;
+
         let button = document.createElement("button");
         button.classList.add("assignmentButton");
         button.type = "button";
         button.innerText = "Accept assignment";
 
+        let notAvailable = document.createElement("p");
+        notAvailable.innerHTML = "Not Available";
+
         div.appendChild(h3);
         div.appendChild(p1);
         div.appendChild(p2);
-        div.appendChild(button);
+        if(isActive === true){
+            div.appendChild(button);
+        }
+        else{
+            div.appendChild(notAvailable);
+        }
         sel.appendChild(div);
 
     }
